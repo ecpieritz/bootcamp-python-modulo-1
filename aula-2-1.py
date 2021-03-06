af0656e8614ae5e89dbfef7067e44dae2841aed3
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2021-03-06 11:08:48 -0300

    aula 2.1 - dicas de boas práticas

---
+++ aula-2-1.py
@@ -0,0 +1,8 @@
+# Dicas de boas práticas em Python
+
+# Aconselhado usar _ para simular o espaço entre palavras nas variáveis
+exemplo_variavel = 'teste de variável'
+print(exemplo_variavel)
+
+# sempre comentar no código
+# deixar instruções se for preciso
