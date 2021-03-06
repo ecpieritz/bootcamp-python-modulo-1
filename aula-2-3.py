cfa424e70c939a812141a509aa05e9fc71228f2c
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2021-03-06 14:06:58 -0300

    2.3 - variáveis

---
+++ aula-2-3.py
@@ -0,0 +1,17 @@
+# variáveis
+
+# Regras básicas para criar variáveis
+
+# 1 - começar com letra
+# 2 - pode usar numero a partir da 2ª
+# 3 - não pode usar simbolo (alem dem _)
+# 4 - não pode conter espaços em branco
+# 5 - não pode ter letras com acentos
+
+#var numérico
+a = 5
+
+#var string (sempre em aspas simples ou duplas)
+b = 'exemplo de string'
+c = "exemplo 2 de string"
+
