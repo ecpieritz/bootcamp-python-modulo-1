df903e6ea4a9f7eeb8c84894788b379094877cac
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2021-03-06 15:04:13 -0300

    3.1 - tipos de dados

---
+++ aula-3-1.py
@@ -0,0 +1,17 @@
+#Tipos de dados
+
+#string
+a = ['exemplo', '45', '@ecpieritz']
+
+#integer (números)
+b = 234
+c = (2, 4, 6, 28)
+
+#float
+d = 45.67
+e =  (12.23, 54.76, 675.1)
+
+#boolean (true ou false)
+number1 = 10 > 5 #dará true
+number2 = 10 > 20 #dará false
+
