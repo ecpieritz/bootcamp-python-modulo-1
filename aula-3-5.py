c172dcce0535edad26179032808a281d89bd5424
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2021-03-07 10:41:13 -0300

    3,5 - operações e converções dos tipos de dados

---
+++ aula-3-5.py
@@ -0,0 +1,27 @@
+#Operações com os tipos de dados
+a = 5
+b = 6
+c = 5
+d = 10
+
+print(a == b)
+print(a == c)
+print(a > b)
+print(a < d)
+print(a >= b)
+print(a <= d)
+print("emilyn" == "caroline")
+
+#convertendo os tipos de dados
+
+e = 500
+f = '200'
+
+g = str(e)
+print(type(g))
+
+h = int(f)
+print(type(h))
+
+i = float(e)
+print(type(i))
