3db0a0517127c1b7f6e69176d158154ab070838e
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2021-03-07 10:20:27 -0300

    3.3 - integer

---
+++ aula-3-3.py
@@ -0,0 +1,20 @@
+#integer
+
+a = 50
+b = 5
+c = 8
+d = -4
+students_number = int(40)
+
+'''
+Divisão de integer + integer = float
+'''
+
+division = a / b
+
+print(division)
+print(type(division)) #sempre será float
+
+division_2 = int(a/b)
+print(division_2)
+print(type(division_2))
