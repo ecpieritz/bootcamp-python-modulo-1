9c6e6cf3a212874cede04ed78382ff8849899c53
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2021-03-08 16:17:25 -0300

    5.2 - if

---
+++ aula-5-2.py
@@ -0,0 +1,28 @@
+#if
+a = 8
+b = 20
+c = 8
+
+#igual
+if a == c:
+    print('A é igual a C')
+
+#diferente
+if a != b:
+    print('A é diferente a B')
+
+#menor que
+if a < b:
+    print('A é menor que B')
+
+#menor ou igual que
+if a <= b:
+    print('A é menor ou igual que B')
+
+#maior que
+if b > c:
+    print('B é maior que C')
+
+#maior ou igual que
+if b >= c:
+    print('B é maior ou igual que C')
