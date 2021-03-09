a4de53cdb3c66cc8734e870f3954b57c802ccf92
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2021-03-09 14:20:33 -0300

    6.2 - while

---
+++ aula-6-2.py
@@ -0,0 +1,20 @@
+#while
+
+num1= 1
+
+while num1 < 10:
+    print(num1)
+    num1 += 1
+
+while num1 < 10:
+    print(num1)
+    if num1 == 6:
+        break
+    num += 1
+
+while num1 < 15:
+    num1 += 1
+    if num1 == 12:
+        continue
+    print(num1)
+
