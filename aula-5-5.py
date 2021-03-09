e57e40a1ba74f870fa06807bdeac9cc9b1392e14
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2021-03-09 11:02:12 -0300

    5.5 - for

---
+++ aula-5-5.py
@@ -0,0 +1,20 @@
+#for
+
+color = ['red', 'yellow', 'brown', 'blue', 'green', 'pink', 'fucsia', 'black', 'white', 'gray', 'purple']
+
+#for em listas
+for x in color:
+    print(x)
+
+#for em strings
+for i in 'Emilyn Caroline':
+    print(i)
+
+#for e break
+
+numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+for x in numbers:
+    print(x)
+    if x == 6:
+        break
