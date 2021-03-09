b5807affa4d0213abadd1b8432a9d5e5cca6d216
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2021-03-09 14:24:32 -0300

    6.3 - range

---
+++ aula-6-3.py
@@ -0,0 +1,14 @@
+#range
+
+#exemplo:
+# range(inicio, termino, salto)
+
+for i in range(1, 21, 3):
+    print(i)
+
+for i in range(0, 51, 5):
+    print(i)
+
+for i in range(0, 101, 10):
+    print(i)
+
